790ca04f8319f3a12df11608e07444aab0cb9015
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-02-01 19:58:31 -0400

    Pagina principal: Commit #2 - 50%-60% Terminado

--- sources/templates/home/index-no-login.php
+++ sources/templates/home/index-no-login.php
@@ -17,14 +17,16 @@
     </div>
 </div>
 <div class="body-container">
-    <div class="div-content-1">
+    <div class="landing-content div-content-1">
         <div class="content-text">
             <div class="text-content">
                 <h1 class="title-landing">Te ayudamos a impulsar tu negocio de forma rápida, sencilla y <span class="text-black">gratis</span>.</h1>
                 <p class="text-landing">Crear un negocio desde cero y darle publicidad puede ser algo tedioso, por eso estamos aqui, para ayudarte a hacer crecer tu negocio sin muchas complicaciones de por medio.</p>
             </div>
-            <div class="link-button-content">
-                <a href="../user/signin.php" class="link-button">Empezar ahora <i class="fa-solid fa-arrow-right-long"></i></a>
+            <div class="link-button">
+                <div class="a-button">
+                    <a href="../user/signin.php">Empezar ahora</a><i class="fa-solid fa-arrow-right-long"></i>
+                </div>
             </div>
         </div>
         <div class="content-img">
