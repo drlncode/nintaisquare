a57a5282b56ad53eb159d597273f641894b1e9df
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-02-02 22:38:29 -0400

    Pagina principal: Commit #4 - 100% Terminada

--- home/index.php
+++ home/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio · NintaiSquare</title>
+    <title>Inicio | NintaiSquare</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
     <link rel="stylesheet" href="../sources/assets/styles/root.css">
     <link rel="stylesheet" href="../sources/assets/styles/index.css">
@@ -14,7 +14,7 @@
     <?php
         if (!isset($_SESSION["USER_AUTH"])) {
             require_once("../sources/templates/home/index-no-login.php");
-            //require_once("../sources/templates/footer/footer.php");
+            require_once("../sources/templates/footer/footer.php");
         } else {
             require_once("../sources/templates/home/index-login.php");
             //require_once("../sources/templates/footer/footer.php");
--- sources/templates/footer/footer.php
+++ sources/templates/footer/footer.php
@@ -1,16 +1,46 @@
 <!---->
 <footer class="footer-container">
-    <div class="footer-content">
-        <div class="fc-div-1">
-
+    <div class="socials-content">
+        <p class="sistema socials-icons">Nuestras redes:</p>
+        <a href="https://github.com/Darlin-code/nintaisquare"><i class="fa-brands fa-github"></i></a>
+        <a href=""><i class="fa-brands fa-instagram"></i></a>
+        <a href=""><i class="fa-brands fa-twitter"></i></a>
+    </div>
+    <div class="infos-content">
+        <div class="ic content-1">
+            <div class="content-title">
+                <h4>Sobre nosotros</h4>
+            </div>
+            <div class="content-links">
+                <a href="">Qué somos</a>
+                <a href="">Quienes somos</a>
+                <a href="">Misión, visión y valores</a>
+                <a href="">Contacto</a>
+            </div>
         </div>
-        <div class="fc-div-2">
-
+        <div class="ic content-2">
+            <div class="content-title">
+                <h4>Descubrir</h4>
+            </div>
+            <div class="content-links">
+                <a href="">Como usar</a>
+                <a href="">Funciones especiales</a>
+                <a href=""></a>
+            </div>
+        </div>
+        <div class="ic content-3">
+            <div class="content-title">
+                <h4>Legal</h4>
+            </div>
+            <div class="content-links">
+                <a href="">Términos y condiciones</a>
+                <a href="">Privacidad</a>
+            </div>
         </div>
     </div>
-    <div class="footer-content-2">
-        <div class="fc-div-3">
-            <i>NintaiSquare - 2023</i>
+    <div class="copy-content">
+        <div class="content-1">
+            <p><i class="fa-regular fa-copyright"></i> 2024 NintaiSquare</p>
         </div>
     </div>
 </footer>
