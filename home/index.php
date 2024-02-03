1e54f675fc039eb653ea111375091a00f3b41f00
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-02-03 10:52:09 -0400

    Darlin: Organizando estilos css

--- home/index.php
+++ home/index.php
@@ -9,8 +9,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio | NintaiSquare</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
-    <link rel="stylesheet" href="../sources/assets/styles/root.css">
-    <link rel="stylesheet" href="../sources/assets/styles/index.css">
+    <?php
+        if (!isset($_SESSION["USER_AUTH"])) { ?>
+            <link rel="stylesheet" href="../sources/assets/styles/root.css">
+            <link rel="stylesheet" href="../sources/assets/styles/index-nl.css">
+        <?php } else { ?>
+            <link rel="stylesheet" href="../sources/assets/styles/root.css">
+            <link rel="stylesheet" href="../sources/assets/styles/index-l.css">
+        <?php }
+    ?>
     <link rel="icon" type="image/x-icon" href="../sources/assets/img/favicon.png">
 </head>
 <body>
