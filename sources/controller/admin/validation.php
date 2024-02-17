4a627d6b9d0e65750d780eccd49fb4013078c062
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-02-17 13:20:27 -0400

    Darlin: Creando el apartado de administración (3/?)

--- sources/controller/admin/validation.php
+++ sources/controller/admin/validation.php
@@ -13,7 +13,7 @@
         } else {
             if (hash("MD5", $_POST["key"]) === $_SESSION["USER_AUTH"]["user_pw"]) {
                 $_SESSION["USER_AUTH"]["admin_confirm"] = true;
-                $_SESSION["msg"] = "<span class='mensaje-success'><i class='fa-solid fa-circle-exclamation'></i>Bienvenido.</span>";
+                $_SESSION["msg"] = "<span class='mensaje-success'><i class='fa-solid fa-circle-check'></i>Bienvenido.</span>";
                 header("Location: ../admin/");
                 return;
             } else {
@@ -31,7 +31,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Validación de administrador | NintaiSquare</title>
     <link rel="stylesheet" href="../../assets/styles/root.css">
-    <link rel="stylesheet" href="../../assets/styles/admin.css">
+    <link rel="stylesheet" href="../../assets/styles/admin-form.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
@@ -47,9 +47,12 @@
                 }
             ?>
             <label for="">
-                <i class="fa-solid fa-key"></i>Introduzca su contraseña: <input type="password" name="key" id="" class="key">
+                <i class="fa-solid fa-key"></i> Introduzca su contraseña: <input type="password" name="key" id="" class="key">
             </label>
-            <button type="submit">Confirmar</button>
+            <div class="botones">
+                <button type="submit">Confirmar</button>
+                <a href="http://localhost/nintaisquare/" class="link-boton">Cancelar</a>
+            </div>
         </form>
     </div>
 </body>
