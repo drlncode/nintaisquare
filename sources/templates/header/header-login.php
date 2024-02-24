d1e34466637e4a2cbe586d15b97633061d699fc8
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-02-24 17:56:51 -0400

    Darlin: Modificando el encabezado (2/2)

--- sources/templates/header/header-login.php
+++ sources/templates/header/header-login.php
@@ -23,7 +23,7 @@
     </div>
     <div class="header-content-2">
         <div class="links">
-            <ul class="nav-container">
+            <ul class="nav-container"> <!-- Encabezado modificado; D: Tiendas, Productos. A: Inicio. -->
                 <li class="sistema nav-link"><a href="https://nintaisquare.com/home/">Inicio</a></li>
                 <li class="sistema nav-link"><a href="https://nintaisquare.com/explore/">Explorar</a></li>
                 <li class="sistema nav-link"><a href="https://nintaisquare.com/create/">Crear</a></li>
