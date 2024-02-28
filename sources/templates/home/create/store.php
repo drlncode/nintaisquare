330759b89dda584f2ca88dd2f6ba10ffccf789f4
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-02-28 09:28:55 -0400

    Darlin: Creando apartado de creación de tiendas y productos (4/?)

---
+++ sources/templates/home/create/store.php
@@ -0,0 +1,19 @@
+<div class="body-container">
+    <div class="body-content">
+        <h2 class="body-title">Registrar tienda.</h2>
+        <form action="" method="post" class="form-container">
+            <label for="store-name">
+                Nombre de la tienda o negocio<input type="text" name="store-name" id="store-name">
+            </label>
+            <select name="categoria" id="categoria">
+                <option value="">-- Elija una opción --</option>
+                <option value="elec">Electrónica</option>
+                <option value="sal">Salud</option>
+                <option value="entret"></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+            </select>
+        </form>
+    </div>
+</div>
