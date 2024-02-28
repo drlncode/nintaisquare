769c30387123e2a80ef3bed77d9ab7a7fed35cde
LUIEMILIO20202020 <luisemiliojavierdelossantos@gmail.com>
2024-02-28 10:11:22 -0400

    Geriel: Creando apartado de creación de tiendas y productos (6/?)

--- sources/templates/home/create/store.php
+++ sources/templates/home/create/store.php
@@ -8,13 +8,36 @@
             <select name="categoria" id="categoria">
                 <option value="">Opciones</option>
                 <hr>
-                <option value="elec">Electrónica</option>
-                <option value="sal">Salud</option>
-                <option value="entret"></option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
+                <option value="electronica">Electrónica</option>
+                <option value="salud">Salud</option>
+                <option value="entretenimiento">Entretenimiento</option>
+                <option value="comidas">Comidas</option>
+                <option value="ropas">Ropas</option>
+                <option value="Otros">Otros</option>
             </select>
+            <label for="">
+                Descripción de la tienda<input type="text" name="Desc" id="Desc">
+            </label>
+            <label for="">
+                Logo de la tienda<input type="file" name="logo" id="logo">
+            </label>
+            <label for="">
+                Dirección de la tienda<input type="text" name="direcc" id="direcc">
+            </label>
+            <label for="">
+                Telefono de la tienda<input type="number" name="tel" id="tel">
+            </label>
+            <label for="">
+                Email de la tienda<input type="email" name="s-email" id="s-email">
+            </label>
+            <div>
+                <label for="">Redes Sociales de la tienda:</label>
+                <input type="text" name="instagram">
+                <input type="text" name="twitter">
+                <input type="text" name="facebook">
+            </div>
+            <button type="submit">Registrar</button>
+            <a href="">Cancelar</a>
         </form>
     </div>
 </div>
