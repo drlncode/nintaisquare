6edd9ecf87ed683729f35261809b937e0164084d
LUIEMILIO20202020 <luisemiliojavierdelossantos@gmail.com>
2024-03-01 11:48:56 -0400

    Luis: Creando apartado de creación de tiendas y productos (14/?)

--- sources/templates/home/create/product.php
+++ sources/templates/home/create/product.php
@@ -3,7 +3,7 @@
         <h2 class="body-title"><i class="fa-solid fa-pen-to-square"></i>Regístrar producto.</h2>
         <form action="" method="post" class="form-container">
             <label for="p-name">
-                Nombre de la tienda<input type="text" name="p-name" id="p-name" placeholder="Nombre de la tienda...">
+                Nombre del producto<input type="text" name="p-name" id="p-name" placeholder="Nombre del producto...">
             </label>
             <label for="p-tienda">Seleccinar la tienda</label>
             <select name="p-tienda" id="p-tienda">
@@ -17,17 +17,8 @@
                 Imagen del producto (Menos de 2MB)<input type="file" name="p-logo" id="p-logo">
             </label>
             <label for="p-desc">
-                Descripción del producto<input type="text" name="p-desc" id="p-desc" placeholder="Descripción breve de la tienda...">
+                Descripción del producto<textarea name="p-desc" id="p-desc" rows="5"></textarea>
             </label>
-            <label for="p-categoria">Categoria del producto</label>
-            <select name="p-categoria" id="p-categoria">
-                <option value="">Seleccione</option>
-                <hr>
-                <option value="tecnologia">Tecnologia</option>
-                <option value="salud">Salud</option>
-                <option value="entretenimiento">Entretenimiento</option>
-                <option value="ropa">Ropa</option>
-            </select>
             <div class="actions">
                 <button type="submit">Registrar</button>
                 <a href="https://nintaisquare.com/create/">Cancelar</a>
