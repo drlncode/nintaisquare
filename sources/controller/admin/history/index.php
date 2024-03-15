7effeccbcdf35869d4ba702344af708981cc0cb7
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-03-14 22:01:45 -0400

    Creando el apartado de administración (23/?)

--- sources/controller/admin/history/index.php
+++ sources/controller/admin/history/index.php
@@ -47,10 +47,49 @@
             <div class="history-container-header">
                 <div class="title-content"><h2 class="title"><i class="fa-solid fa-clock-rotate-left"></i>Historial</h2></div>
             </div>
-            <div class="hostory-register-container">
-                <div class="hostory-register">
-                    
-                </div>
+            <div class="history-register-container">
+                <?php
+                    $query = $pdo -> query("SELECT * FROM history;");
+                    $query_n = $pdo -> prepare("SELECT name FROM users WHERE user_id = :id");
+
+                    while ($history = $query -> fetch(PDO::FETCH_ASSOC)) {
+                        $query_n -> execute(array(
+                            ':id' => $history["by"]
+                        ));
+                        $pre_name = $query_n -> fetch(PDO::FETCH_ASSOC);
+                        $name = explode(" ", $pre_name["name"]);
+                        
+                        if ($history["status"] == "accepted" && $history["category"] == "store") { ?>
+                            <div class="history-register accepted">
+                                <div class="date"><span><?= $history["date"] ?></span></div>
+                                <div class="info">
+                                    <span><?= $name[0] ?> aprobó una tienda del usuario con el ID: <a href="https://nintaisquare.com/user/profile.php?user_id=<?= $history["of"] ?>" target="_blank"><?= $history["of"] ?></a></span>
+                                </div>
+                            </div>
+                        <?php } elseif ($history["status"] == "accepted" && $history["category"] == "product") { ?>
+                            <div class="history-register accepted">
+                                <div class="date"><span><?= $history["date"] ?></span></div>
+                                <div class="info">
+                                    <span><?= $name[0] ?> aprobó un producto de la tienda con el ID: <?= $history["of"] ?></span>
+                                </div>
+                            </div>
+                        <?php } elseif ($history["status"] == "denied" && $history["category"] == "store") { ?>
+                            <div class="history-register denied">
+                                <div class="date"><span><?= $history["date"] ?></span></div>
+                                <div class="info">
+                                    <span><?= $name[0] ?> rechazó una tienda del usuario con el ID: <a href="https://nintaisquare.com/user/profile.php?user_id=<?= $history["of"] ?>" target="_blank"><?= $history["of"] ?></a></span>
+                                </div>
+                            </div>
+                        <?php } elseif ($history["status"] == "denied" && $history["category"] == "product") { ?>
+                            <div class="history-register denied">
+                                <div class="date"><span><?= $history["date"] ?></span></div>
+                                <div class="info">
+                                    <span><?= $name[0] ?> rechazó un producto de la tienda con el ID: <?= $history["of"] ?></span>
+                                </div>
+                            </div>
+                        <?php }
+                    }
+                ?>
             </div>
         </div>
     </div>
