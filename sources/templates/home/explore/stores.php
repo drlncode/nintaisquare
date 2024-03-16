752f44d69084bf34391a53e61914a74731def52a
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-03-16 14:29:35 -0400

    Darlin: Creando apartado de exploración (4/?)

---
+++ sources/templates/home/explore/stores.php
@@ -0,0 +1,50 @@
+<div class="content-stores">
+    <div class="filter-container-stores">
+        <div class="title-filter">
+            <h2 class="title"><i class="fa-solid fa-filter"></i>Filtar por:</h2>
+        </div>
+        <div class="filter-options-container">
+            <form action="" method="get">
+                <div class="sub-title-filter">
+                    <h2>Categoria</h2>
+                </div>
+                <fieldset class="fieldset-container">
+                    <legend class="fieldset-title">Seleccione una categoria</legend>
+                    <div class="filter-options">
+                        <label for="entretenimiento">
+                            <input type="radio" name="categoria" value="entretenimiento" id="entretenimiento" checked>Entretenimiento
+                        </label>
+                        <label for="comida">
+                            <input type="radio" name="categoria" value="comida" id="comida">Comida
+                        </label>
+                        <label for="salud">
+                            <input type="radio" name="categoria" value="salud" id="salud">Salud
+                        </label>
+                        <label for="ropas">
+                            <input type="radio" name="categoria" value="ropas" id="ropas">Ropas
+                        </label>
+                        <label for="tecnologia">
+                            <input type="radio" name="categoria" value="tecnologia" id="tecnologia">Tecnología
+                        </label>
+                        <label for="canasta-basica">
+                            <input type="radio" name="categoria" value="canasta-basica" id="canasta-basica">Canasta básica
+                        </label>
+                        <label for="mecanica">
+                            <input type="radio" name="categoria" value="mecanica" id="mecanica">Mecánica
+                        </label>
+                        <label for="ventas-generales">
+                            <input type="radio" name="categoria" value="ventas-generales" id="ventas-generales">Ventas generales
+                        </label>
+                    </div>
+                </fieldset>
+                <div class="filter-button">
+                    <div class="div-space"></div>
+                    <button class="sort" type="submit">Filtar<i class="fa-solid fa-filter"></i></button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="results-stores">
+        
+    </div>
+</div>
