16e36073be64d57a7856a9e7efbcc4528785588a
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-03-15 20:29:42 -0400

    Darlin: Estableciendo permisos inexistentes.

--- explore/index.php
+++ explore/index.php
@@ -2,6 +2,7 @@
     session_start();
     require_once("../sources/controller/funciones.php");
     require_once("../sources/controller/pdo.php");
+    noset();
 ?>
 <!DOCTYPE html>
 <html lang="es">
--- search/index.php
+++ search/index.php
@@ -1,4 +1,6 @@
 <?php
     session_start();
+    require_once("../sources/controller/funciones.php");
     require_once("../sources/controller/pdo.php");
+    noset();
 ?>
