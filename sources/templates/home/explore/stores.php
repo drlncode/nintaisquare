75093cc2d4f749172ba30016e4f6ee18de70c183
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-03-17 00:49:25 -0400

    Darlin: Creando apartado de exploración (5/?)

--- sources/templates/home/explore/stores.php
+++ sources/templates/home/explore/stores.php
@@ -1,7 +1,7 @@
 <div class="content-stores">
     <div class="filter-container-stores">
         <div class="title-filter">
-            <h2 class="title"><i class="fa-solid fa-filter"></i>Filtar por:</h2>
+            <h2 class="title"><i class="fa-solid fa-filter"></i>Filtrar por:</h2>
         </div>
         <div class="filter-options-container">
             <form action="" method="get">
@@ -37,14 +37,49 @@
                         </label>
                     </div>
                 </fieldset>
+                <div class="sub-title-filter">
+                    <h2>Creación</h2>
+                </div>
+                <fieldset class="fieldset-container">
+                    <legend class="fieldset-title">Seleccione una opcion</legend>
+                    <div class="filter-options">
+                        <label for="newest">
+                            <input type="radio" name="longevidad" value="newest" id="newest" checked>Más nuevo
+                        </label>
+                        <label for="older">
+                            <input type="radio" name="longevidad" value="older" id="older">Más antiguo
+                        </label>
+                    </div>
+                </fieldset>
                 <div class="filter-button">
                     <div class="div-space"></div>
-                    <button class="sort" type="submit">Filtar<i class="fa-solid fa-filter"></i></button>
+                    <button class="sort" type="submit">Filtrar<i class="fa-solid fa-filter"></i></button>
                 </div>
             </form>
         </div>
     </div>
     <div class="results-stores">
-        
+        <a href="">
+            <div class="result">
+                <div class="result-content-1">
+                    <div class="img-result">
+                        <img src="../sources/assets/img/logo-socials.jpg" alt=""/>
+                    </div>
+                    <div class="title-category-result">
+                        <div class="title-result">
+                            <span class="title"><b>NintaiSquare</b></span>
+                        </div>
+                        <div class="category-result">
+                            <span class="category">Categoria: <b>Tecnología</b></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="result-content-2">
+                    <div class="desc-result">
+                        <span class="desc">Te ayudamos a impulsar tu negocio de forma rápida, sencilla y gratis. Dar a conocer tu negocio nunca fue tan fácil.</span>
+                    </div>
+                </div>
+            </div>
+        </a>
     </div>
 </div>
