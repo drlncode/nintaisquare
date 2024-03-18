a56500376398ce7d5eddb1238018d008eacccf63
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-03-18 13:23:30 -0400

    Darlin: Ajustando la página inicial.

--- sources/templates/home/index-login.php
+++ sources/templates/home/index-login.php
@@ -10,17 +10,17 @@
         </div>
         <div class="links-button">
             <div class="link-button button-1">
-                <a href="https://nintaisquare.com/explore/" class="button">Explorar <i class="fa-solid fa-arrow-right-long"></i></a>
+                <a href="https://nintaisquare.com/explore/" class="button">Explorar<i class="fa-solid fa-arrow-right-long"></i></a>
             </div>
             <div class="link-button button-2">
-                <a href="https://nintaisquare.com/create/" class="button">Registrar mi tienda <i class="fa-solid fa-arrow-right-long"></i></a>
+                <a href="https://nintaisquare.com/create/" class="button">Registrar<i class="fa-solid fa-arrow-right-long"></i></a>
             </div>
         </div>
     </div>
     <div class="content-2">
         <div class="text-container">
             <div class="title-content">
-                <h3 class="sub-title"><i class="fa-solid fa-map-location-dot"></i> Explorar</h3>
+                <h3 class="sub-title"><i class="fa-solid fa-map-location-dot"></i>Explorar</h3>
             </div>
             <div class="text">
                 <p>Descubre todo tipo de tiendas interesantes y los productos que te ofrecen. Puedes ver las diferentes tiendas registradas ordenadas por categoria, asi como tambien todos sus productos ordenados por categoria.</p>
@@ -28,7 +28,7 @@
         </div>
         <div class="text-container">
             <div class="title-content">
-                <h3 class="sub-title"><i class="fa-solid fa-shop"></i> Registrar tienda</h3>
+                <h3 class="sub-title"><i class="fa-solid fa-shop"></i>Registrar tienda</h3>
             </div>
             <div class="text">
                 <p>Registra tu tienda de forma gratuita para que las personas puedan ver su lista de productos. Tambien asegurate de seguir las normas establecidas para poder registrarla correctamente y dar a conocer tu tienda de la mejor forma posible.</p>
