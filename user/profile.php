f7ab6068c5e91d460e73a8f4fe337a10f003bbb8
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-03-20 19:23:48 -0400

    Darlin: Creando el apartado de perfil del usuario (3/?)

--- user/profile.php
+++ user/profile.php
@@ -32,6 +32,13 @@
             }
         }
     }
+
+    $query = $pdo -> prepare("SELECT count(*) registradas FROM val_stores WHERE user_id = :id");
+    $query -> execute(array(
+        ':id' => $_GET["user_id"]
+    ));
+    $registradas = $query -> fetch(PDO::FETCH_ASSOC);
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -41,46 +48,76 @@
     <title><?= $user_data["name"] ?> | NintaiSquare</title>
     <link rel="icon" type="image/x-icon" href="../sources/assets/img/favicon.png">
     <link rel="stylesheet" href="../sources/assets/styles/root.css">
-    <link rel="stylesheet" href="../sources/assets/styles/index-l.css">
     <link rel="stylesheet" href="../sources/assets/styles/user.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-    <?= require_once("../sources/templates/header/header-login.php"); ?>
-    <div class="profile-user-container">
-        <div class="profile-user-container-title">
-            <h1 class="title"><?= $user -> own_profile() ? "Mi perfil" : "Perfil"?></h1>
-        </div>
-        <div class="profile-user-content">
-            <div class="profile-user-pic">
-                <img src="../sources/assets/img/user-icon.svg" alt="User-icon">
+    <div class="container">
+        <?php 
+            require_once("../sources/templates/header/header-login.php"); 
+        ?>
+        <div class="user-content">
+            <div class="user-name">
+                <h1 class="name"><?= $user -> own_profile() ? "Mi perfil" : "Perfil"?></h1>
             </div>
-            <div class="profile-user-data">
-                <h4 class="profile-user-name"><?= $user_data["name"]; ?></h4>
-                <div class="profile-user-details">
-                    <p class="details-text">Tiendas registradas: <!-- En proceso... --></p>
+            <div class="user-data-content">
+                <div class="profile-user-pic">
+                    <img class="pic" src="../sources/assets/img/user-icon.svg" alt="User-icon">
+                </div>
+                <div class="profile-user-desc">
+                    <h4 class="profile-user-name"><?= $user_data["name"]; ?></h4>
+                    <div class="profile-user-details">
+                        <p class="details-text">Tiendas registradas: <?= $registradas["registradas"] ?></p>
+                    </div>
+                </div>
+                <div class="actions">
+                <?php
+                    if ($user_data["admin"] == 1) { ?>
+                        <div class="user-rol"><p class="admin-text">Administrador</p></div>
+                    <?php } else { ?>
+                        <div class="user-rol"><p class="user-text">Usuario</p></div>
+                    <?php }
+                ?>
+                <?php
+                    if ($user -> own_profile()) { ?>
+                        <div class="profile-user-action">
+                            <div class="link"><a href="https://nintaisquare.com/user/settings.php"><i class="fa-solid fa-gear"></i>Ajustes</a></div>
+                        </div>
+                    <?php }
+                ?>
                 </div>
             </div>
-            <?php
-                if ($user_data["admin"] == 1) { ?>
-                    <div class="user-rol"><p class="admin-text">Administrador</p></div>
-                <?php } else { ?>
-                    <div class="user-rol"><p class="user-text">Usuario</p></div>
-                <?php }
-            ?>
-            <?php
-                if ($user -> own_profile()) { ?>
-                    <div class="profile-user-action">
-                        <div class="link"><a href="https://nintaisquare.com/user/settings.php"><i class="fa-solid fa-gear"></i>Ajustes</a></div>
-                    </div>
-                <?php }
-            ?>
+            <div class="user-stores-content">
+                <h2 class="h2-title"><?= $user -> own_profile() ? "Mis tiendas" : "Tiendas"?></h2>
+                <div class="user-stores">
+                    <?php
+                        $query = $pdo -> prepare("SELECT * FROM val_stores WHERE user_id = :id");
+                        $query -> execute(array(
+                            ':id' => $_GET["user_id"]
+                        ));
+                        
+                        while ($user_stores = $query -> fetch(PDO::FETCH_ASSOC)) { ?>
+                            <a href="">
+                                <div class="store">
+                                    <div class="divisor">
+                                        <span>Tienda de <?= prettyCategory($user_stores["store_category"]) ?></span>
+                                    </div>
+                                    <div class="data">
+                                        <div class="store-img">
+                                            <img src="data:image/png;base64,<?= $user_stores["store_img"] ?>" alt="">
+                                        </div>
+                                        <div class="store-name"><span class="title"><?= $user_stores["store_name"] ?></span></div>
+                                    </div>
+                                </div>
+                            </a>
+                        <?php }
+                    ?>
+                </div>
+            </div>
         </div>
-        <div class="user-stores-content">
-            <h3 class="h3-title"><?= $user -> own_profile() ? "Mis tiendas" : "Tiendas"?></h3>
-            <!-- En proceso... -->
-        </div>
+        <?php 
+            require_once("../sources/templates/footer/footer.php");
+        ?>
     </div>
-    <?php require_once("../sources/templates/footer/footer.php");?>
 </body>
 </html>
