872b5f2f485c0142f30f481f2afa8c3faa9b3fb8
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-03-21 17:38:41 -0400

    Darlin: Corrigiendo ERROR 500.

--- sources/controller/pdo.php
+++ sources/controller/pdo.php
@@ -1,4 +1,4 @@
 <?php
-    //$pdo = new PDO("mysql:host=localhost; dbname=u433460052_nintaisquare", "u433460052_root", "Darlin_123");
-    $pdo = new PDO("mysql:host=localhost; dbname=nintaisquare", "root", "Darlin_123");
+    $pdo = new PDO("mysql:host=localhost; dbname=u433460052_nintaisquare", "u433460052_root", "Darlin_123");
+    //$pdo = new PDO("mysql:host=localhost; dbname=nintaisquare", "root", "Darlin_123");
 ?>
