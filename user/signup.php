6686ff9ad12d610b9e96ddf407b3c3424e8e3b0d
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-03-22 18:59:14 -0400

    Darlin: Incorporando el no-responsive (2/?)

--- user/signup.php
+++ user/signup.php
@@ -51,9 +51,13 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
     <link rel="stylesheet" href="../sources/assets/styles/root.css">
     <link rel="stylesheet" href="../sources/assets/styles/styles-form.css">
+    <link rel="stylesheet" href="../sources/assets/styles/no-responsive.css">
     <link rel="icon" type="image/x-icon" href="../sources/assets/img/favicon.png">
 </head>
 <body>
+    <?php
+        require_once("../sources/templates/no-resposive/index.php");
+    ?>
     <div class="container">
         <div class="form-container-1">
             <div class="form-content-1">
