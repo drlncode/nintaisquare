1d562ee5f179469383bf1136a42c0c70a89f6fa5
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-03-21 23:17:53 -0400

    Darlin: Creando apartado de tiendas (2/?)

--- store/index.php
+++ store/index.php
@@ -3,15 +3,32 @@
     require_once("../sources/controller/pdo.php");
     require_once("../sources/controller/funciones.php");
     noset();
+
+    if (!isset($_GET["store_id"]) || isset($_GET["store_id"]) && empty($_GET["store_id"])) {
+        header("Location: https://nintaisquare.com/explore/");
+        exit;
+    } else {
+        $query = $pdo -> prepare("SELECT * FROM val_stores WHERE store_id = :id");
+        $query -> execute(array(
+            ':id' => $_GET["store_id"]
+        ));
+        $store = $query -> fetch(PDO::FETCH_ASSOC);
+
+        if ($store === false) {
+            header("Location: https://nintaisquare.com/explore/");
+            exit;
+        }
+    }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>| NintaiSquare</title>
+    <title><?= $store["store_name"]; ?> | NintaiSquare</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
     <link rel="stylesheet" href="../sources/assets/styles/root.css">
+    <link rel="stylesheet" href="../sources/assets/styles/store.css">
     <link rel="stylesheet" href="../sources/assets/styles/index-l.css">
     <link rel="stylesheet" href="../sources/assets/styles/no-responsive.css">
     <link rel="shortcut icon" href="../sources/assets/img/favicon.png" type="image/x-icon">
@@ -24,7 +41,80 @@
         <?php
             require_once("../sources/templates/header/header-login.php");
         ?>
+        <div class="store-content">
+            <div class="header">
+                <h2 class="title"><i class="fa-solid fa-shop"></i>Tienda</h2>
+            </div>
+            <div class="store">
+                <div class="content-1">
+                    <div class="store-img">
+                        <img class="img" src="data:image/png;base64,<?= $store["store_img"]; ?>" title="<?= $store["store_name"]; ?>">
+                    </div>
+                    <div class="details">
+                        <div class="name-category">
+                            <h3 class="name"><i class="fa-solid fa-pen-to-square"></i><?= $store["store_name"]; ?></h3>
+                            <span class="category"><?= prettyCategory($store["store_category"]); ?></span>
+                        </div>
+                        <div class="description">
+                            <span class="description-text"><i class="fa-solid fa-comment"></i><?= $store["store_desc"] ?></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="content-2">
+                    <div class="store-data">
+                        <div class="title-data">
+                            <h4 class="title title-h4"><i class="fa-solid fa-circle-info"></i>Ponte en contacto con esta tienda</h4>
+                        </div>
+                        <div class="phone-email">
+                            <div class="phone">
+                                <span class="text-plane phone-text"><i class="fa-solid fa-phone"></i><?= $store["store_tel"] ?></span>
+                            </div>
+                            <?php 
+                                if (!empty($store["store_email"])) { ?>
+                                    <div class="email">
+                                        <span class="text-plane email-text"><i class="fa-solid fa-envelope"></i><?= $store["store_email"] ?></span>
+                                    </div>
+                                <?php }
+                            ?>
+                        </div>
+                        <div class="socials-container">
+                            <?php
+                                if (!empty($store["store_social_ig"]) || !empty($store["store_social_tw"]) || !empty($store["store_social_fc"])) { ?>
+                                    <div class="title-socials">
+                                        <h4 class="title title-h4"><i class="fa-solid fa-heart"></i>También puedes encontrar esta tienda en</h4>
+                                    </div>
+                                    <div class="socials-links">
+                                        <?php 
+                                            if (!empty($store["store_social_ig"])) { ?>
+                                                <div class="links">
+                                                    <a href="<?= $store["store_social_ig"] ?>" class="ig-link" target="_blank"><i class="fa-brands fa-instagram"></i>Instagram</a>
+                                                </div>
+                                            <?php }
+                                            if (!empty($store["store_social_tw"])) { ?>
+                                                <div class="links">
+                                                    <a href="<?= $store["store_social_tw"] ?>" class="tw-link" target="_blank"><i class="fa-brands fa-x-twitter"></i>Twitter</a>
+                                                </div>
+                                            <?php }
+                                            if (!empty($store["store_social_fc"])) { ?>
+                                                <div class="links">
+                                                    <a href="<?= $store["store_social_fc"] ?>" class="fc-link" target="_blank"><i class="fa-brands fa-facebook"></i>Facebook</a>
+                                                </div>
+                                            <?php }
+                                        ?>
+                                    </div>
+                                <?php }
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="store-products">
 
+            </div>
+            <div class="like-this">
+
+            </div>
+        </div>
         <?php
             require_once("../sources/templates/footer/footer.php");
         ?>
