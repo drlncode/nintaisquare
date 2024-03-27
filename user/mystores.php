ac851a13689b74dd6a159dc31d930ab177140713
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-03-27 14:18:54 -0400

    Darlin: Creando el apartado de tiendas del usuario. (1/?)

---
+++ user/mystores.php
@@ -0,0 +1,43 @@
+<?php
+    session_start();
+    require_once("../sources/controller/pdo.php");
+    require_once("../sources/controller/funciones.php");
+    noset();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mis tiendas | NintaiSquare</title>
+    <link rel="icon" type="image/x-icon" href="../sources/assets/img/favicon.png">
+    <link rel="stylesheet" href="../sources/assets/styles/root.css">
+    <link rel="stylesheet" href="../sources/assets/styles/index-l.css">
+    <link rel="stylesheet" href="../sources/assets/styles/user-stores.css">
+    <link rel="stylesheet" href="../sources/assets/styles/no-responsive.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <?php
+        require_once("../sources/templates/no-resposive/index.php");
+    ?>
+    <div class="container">
+        <?php
+            require_once("../sources/templates/header/header-login.php");
+        ?>
+        <div class="user-stores-container">
+            <div class="user-stores-header">
+                <h2 class="title"><i class="fa-solid fa-shop"></i>Mis tiendas</h2>
+            </div>
+            <div class="user-stores-content">
+                <div class="user-store">
+
+                </div>
+            </div>
+        </div>
+        <?php
+            require_once("../sources/templates/footer/footer.php");
+        ?>
+    </div>
+</body>
+</html>
