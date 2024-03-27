5f7f780480f45df34ac6610df743f88b6f3b6c5e
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-03-27 18:44:04 -0400

    Darlin: Creando el apartado de tiendas del usuario. (2/?)

--- user/mystores.php
+++ user/mystores.php
@@ -3,6 +3,16 @@
     require_once("../sources/controller/pdo.php");
     require_once("../sources/controller/funciones.php");
     noset();
+
+    $query_val = $pdo -> prepare("SELECT * FROM val_stores WHERE user_id = :id;");
+    $query_val -> execute(array(
+        ':id' => $_SESSION["USER_AUTH"]["user_id"]
+    ));
+
+    $query_pen = $pdo -> prepare("SELECT * FROM pen_stores WHERE user_id = :id;");
+    $query_pen -> execute(array(
+        ':id' => $_SESSION["USER_AUTH"]["user_id"]
+    ));
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -24,15 +34,39 @@
     <div class="container">
         <?php
             require_once("../sources/templates/header/header-login.php");
+
+            if ($query_pen -> rowCount() != 0) { $earring = $query_pen -> fetch(PDO::FETCH_ASSOC); ?>
+                <div class="earrings">
+                    <div class="counter"><span class="count"><?= $query_pen -> rowCount(); ?></span></div>
+                    <a href="?tabFor" class="tabFor-link">Tiendas pendientes<i class="fa-solid fa-angle-right"></i></a>
+                </div>
+                <div class="tabFor" style="<?= isset($_GET["tabFor"]) ? 'display: block;' : 'display: none;' ?>"></div>
+            <?php }
         ?>
         <div class="user-stores-container">
             <div class="user-stores-header">
                 <h2 class="title"><i class="fa-solid fa-shop"></i>Mis tiendas</h2>
             </div>
             <div class="user-stores-content">
-                <div class="user-store">
-
-                </div>
+                <?php
+                    if ($query_val -> rowCount() == 0) { ?>
+                        <span class="no-stores"></span>
+                    <?php } else {
+                        while ($store = $query_val -> fetch(PDO::FETCH_ASSOC)) { ?>
+                            <div class="user-store">
+                                <div class="store-category">
+                                    <span class="category"><?= prettyCategory($store["store_category"]) ?></span>
+                                </div>
+                                <div class="store-img">
+                                    <img src="data:img/png;base64,<?= $store["store_img"] ?>" alt="<?= $store["store_name"] ?>">
+                                </div>
+                                <div class="store-name">
+                                    <span class="name"><?= $store["store_name"] ?></span>
+                                </div>
+                            </div>
+                        <?php }
+                    }
+                ?>
             </div>
         </div>
         <?php
