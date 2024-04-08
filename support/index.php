3a52c9e1d091746275f1477ca6f28bc9018ece65
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-04-08 02:24:37 -0400

    Darlin: Creando apartado de soporte (3.6/?)

--- support/index.php
+++ support/index.php
@@ -11,9 +11,9 @@
     <meta name="description" content="Soporte NintaiSquare">
     <title>Soporte | NintaiSquare</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
-    <link rel="stylesheet" href="https://nintaisquare.com/sources/assets/styles/root.css">
-    <link rel="stylesheet" href="https://nintaisquare.com/sources/assets/styles/support.css">
-    <link rel="icon" type="image/x-icon" href="https://nintaisquare.com/sources/assets/img/favicon-support.png">
+    <link rel="stylesheet" href="../source/styles/root.css">
+    <link rel="stylesheet" href="../source/styles/support.css">
+    <link rel="icon" type="image/x-icon" href="../source/img/favicon-support.png">
 </head>
 <body>
     <div class="container">
