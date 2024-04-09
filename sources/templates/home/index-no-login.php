b23c72093b866344383b6f37be0b6fa68c442e45
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-04-09 16:10:31 -0400

    Darlin: Mejorando la relevancia de la página.

--- sources/templates/home/index-no-login.php
+++ sources/templates/home/index-no-login.php
@@ -2,9 +2,9 @@
     <div class="landing-content div-content-1">
         <div class="content-text">
             <div class="text-content">
-                <h1 class="main-title title-landing">NintaiSquare</h1>
-                <h2 class="title-landing">Te ayudamos a impulsar tu negocio de forma rápida, sencilla y <span class="text-black">gratis</span>.</h2>
-                <p class="text-landing">Crear un negocio desde cero y darle publicidad puede ser algo tedioso, por eso estamos aqui, para ayudarte a hacer crecer tu negocio sin muchas complicaciones de por medio.</p>
+                <h1 class="main-title title-landing"><strong>NintaiSquare</strong></h1>
+                <h2 class="title-landing"><strong>Te ayudamos a impulsar tu negocio de forma rápida, sencilla y <span class="text-black">gratis</span>.</strong></h2>
+                <p class="text-landing">Crear un negocio desde cero y darle publicidad puede ser algo tedioso, por eso estamos aqui, para ayudarte a hacer crecer tu negocio de manera gratuita sin muchas complicaciones de por medio.</p>
             </div>
             <div class="link-button">
                 <a href="https://nintaisquare.com/user/signin.php" class="sistema button">Empezar ahora <i class="fa-solid fa-arrow-right-long"></i></a>
--- support/index.php
+++ support/index.php
@@ -9,6 +9,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Soporte NintaiSquare">
+    <meta name="description" content="Te damos la bienvenida a la sección de soporte de NintaiSquare">
     <title>Soporte | NintaiSquare</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
     <link rel="stylesheet" href="../sources/assets/styles/root.css">
