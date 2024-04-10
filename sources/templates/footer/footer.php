95235861055fdaa05533e3e08471ad4b3d1a5801
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-04-09 22:17:03 -0400

    Darlin: Creando apartado de soporte (8/?)

--- sources/templates/footer/footer.php
+++ sources/templates/footer/footer.php
@@ -22,9 +22,9 @@
                 <h4>Soporte</h4>
             </div>
             <div class="content-links">
-                <a href="https://support.nintaisquare.com/">Preguntas frecuentes</a>
-                <a href="https://support.nintaisquare.com/">Recuperar cuenta</a>
-                <a href="https://support.nintaisquare.com/">Reportar error</a>
+                <a href="https://nintaisquare.com/#frecuent-questions">Preguntas frecuentes</a>
+                <a href="https://nintaisquare.com/">Recuperar cuenta</a>
+                <a href="https://nintaisquare.com/">Reportar error</a>
             </div>
         </div>
     </div>
--- support/index.php
+++ support/index.php
@@ -32,7 +32,7 @@
             <div class="user">
                 <?php
                     if (!isset($_SESSION["USER_AUTH"])) { ?>
-                        <a href="https://nintaisquare.com/user/signin.php?support" class="login" target="_blank"><i class="fa-solid fa-right-to-bracket"></i>Iniciar sesión</a>
+                        <a href="https://nintaisquare.com/user/signin.php?support" class="login"><i class="fa-solid fa-right-to-bracket"></i>Iniciar sesión</a>
                     <?php } else { ?>
                         <a href="https://nintaisquare.com/user/profile.php?user_id=<?= $_SESSION["USER_AUTH"]["user_id"] ?>" class="account" target="_blank" title="<?= $_SESSION["USER_AUTH"]["name"] ?>">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M399 384.2C376.9 345.8 335.4 320 288 320H224c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z"/></svg>
@@ -44,10 +44,42 @@
         </div>
         <div class="content">
             <div class="sub-header-container">
-                <h1 class="sub-header-title">Soporte NintaiSquare</h1>
+                <h1 class="sub-header-title"><i class="fa-solid fa-headset"></i>Soporte</h1>
                 <span class="sub-header-text">Te damos la bienvenida a la sección de soporte de NintaiSquare, aqui podrás ponerte en contacto con nuestro personal para resolver tus dudas o incovenientes.</span>
             </div>
-            <div class="support-questions">
+            <div class="options-container">
+                <a href="">
+                    <div class="option contact-us">
+                        <div class="option-title">
+                            <h3 class="title"><i class="fa-solid fa-comments"></i>Contáctanos</h3>
+                        </div>
+                        <div class="option-content">
+                            <span class="text">Podrás descubrir como ponerte en contacto con nuestro equipo.</span>
+                        </div>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="option report-something">
+                        <div class="option-title">
+                            <h3 class="title"><i class="fa-solid fa-triangle-exclamation"></i>Reportar un error</h3>
+                        </div>
+                        <div class="option-content">
+                            <span class="text">Reporta cualquier error que surga en la plataforma para su pronta solución.</span>
+                        </div>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="option how-use">
+                        <div class="option-title">
+                            <h3 class="title"><i class="fa-solid fa-circle-question"></i>Cómo utilizar</h3>
+                        </div>
+                        <div class="option-content">
+                            <span class="text">Descubre todo acerca de como utilizar NintaiSquare y sus funcionalidades.</span>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="support-questions" id="frecuent-questions">
                 <h2 class="title"><i class="fa-regular fa-circle-question"></i>Preguntas frecuentes</h2>
                 <div class="questions">
                     <details>
@@ -76,10 +108,12 @@
         </div>
     </div>
     <footer class="footer-container">
-            <div class="img">
-                <img src="../sources/assets/img/logo-blanco.png" alt="">
-            </div>
-            <div class="text"><i class="fa-regular fa-copyright"></i>NintaiSquare, todos los derechos reservados.</div>
+            <a href="https://nintaisquare.com/">
+                <div class="img">
+                    <img src="../sources/assets/img/logo-blanco.png" alt="NintaiSquare Logo" title="NintaiSquare">
+                </div>
+            </a>
+            <div class="text"><i class="fa-regular fa-copyright"></i>NintaiSquare - Todos los derechos reservados.</div>
             <div class="socials">
                 <a href="https://github.com/Darlin-code/nintaisquare" target="_blank" title="Github - NintaiSquare"><i class="fa-brands fa-github"></i></a>
                 <a href="https://www.instagram.com/nintaisquare/" target="_blank" title="Instagram - NintaiSquare"><i class="fa-brands fa-instagram"></i></a>
