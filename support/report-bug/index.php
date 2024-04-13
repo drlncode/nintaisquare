06a5cc2b7fbbe8acbb14983608f80162bb36fddb
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-04-13 16:11:42 -0400

    Darlin: Creando apartado de soporte (15/?)

--- support/report-bug/index.php
+++ support/report-bug/index.php
@@ -8,7 +8,31 @@
             $_SESSION["msg"] = "<span class='mensaje-error'><i class='fa-solid fa-circle-exclamation'></i>Rellene todos los campos.</span>";
             header("Location: " . $_SERVER["REQUEST_URI"] . "");
             exit;
-        } elseif (isset($_POST["e-r-email"])) {
+        }
+        if ($_FILES["e-img"]["size"] != 0) {
+            if ($_FILES["e-img"]["size"] / 1024 > 2048) {
+                $_SESSION["msg"] = "<span class='mensaje-error'><i class='fa-solid fa-circle-exclamation'></i>La imagen excede el limite de 2MB.</span>";
+                header("Location: " . $_SERVER["REQUEST_URI"] . "");
+                exit;
+            } elseif ($_FILES["e-img"]["type"] !== "image/png" && $_FILES["e-img"]["type"] !== "image/jpg" && $_FILES["e-img"]["type"] !== "image/jpeg") {
+                $_SESSION["msg"] = "<span class='mensaje-error'><i class='fa-solid fa-circle-exclamation'></i>Seleccione un formato de imagen válido.</span>";
+                header("Location: " . $_SERVER["REQUEST_URI"] . "");
+                exit;
+            }
+        } 
+        if (strlen($_POST["e-desc"]) < 100) {
+            $_SESSION["cache"] = $_POST["e-desc"];
+            $_SESSION["msg"] = "<span class='mensaje-error'><i class='fa-solid fa-circle-exclamation'></i>No cumple con el mínimo de carácteres.</span>";
+            header("Location: " . $_SERVER["REQUEST_URI"] . "");
+            exit;
+        } 
+        if (strlen($_POST["e-desc"]) > 512) {
+            $_SESSION["cache"] = $_POST["e-desc"];
+            $_SESSION["msg"] = "<span class='mensaje-error'><i class='fa-solid fa-circle-exclamation'></i>Se excede con el máximo de carácteres.</span>";
+            header("Location: " . $_SERVER["REQUEST_URI"] . "");
+            exit;
+        } 
+        if (isset($_POST["e-r-email"])) {
             if (empty($_POST["e-r-email"])) {
                 $_SESSION["msg"] = "<span class='mensaje-error'><i class='fa-solid fa-circle-exclamation'></i>Ingrese un correo.</span>";
                 header("Location: " . $_SERVER["REQUEST_URI"] . "");
@@ -18,11 +42,6 @@
                 header("Location: " . $_SERVER["REQUEST_URI"] . "");
                 exit;
             }
-        } elseif (strlen($_POST["e-desc"]) > 512) {
-            $_SESSION["cache"] = $_POST["e-r-email"];
-            $_SESSION["msg"] = "<span class='mensaje-error'><i class='fa-solid fa-circle-exclamation'></i>La descripción excede el máximo de carácteres.</span>";
-            header("Location: " . $_SERVER["REQUEST_URI"] . "");
-            exit;
         }
     }
 ?>
@@ -31,7 +50,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Contácto NintaiSquare">
+    <meta name="description" content="Reportar NintaiSquare">
     <title>Reportar error | NintaiSquare</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
     <link rel="stylesheet" href="../../sources/assets/styles/root.css">
@@ -52,9 +71,9 @@
             <div class="header">
                 <h2 class="title"><i class="fa-solid fa-bug"></i>Reportar un error</h2>
             </div>
-            <form action="" method="post" class="report-form-content">
+            <form action="" method="post" class="report-form-content" enctype="multipart/form-data">
                 <label for="e-category"><p class="caption">Selecciona el problema</p>
-                    <select name="e-category" id="e-category" autofocus="autofocus">
+                    <select name="e-category" id="e-category" <?= !isset($_SESSION["cache"]) ? 'autofocus="autofocus"' : '' ?>>
                         <option value="">Seleccione</option><hr class="new">
                         <option value="e-d">Error de diseño</option>
                         <option value="e-r">Error al registrar</option>
@@ -63,8 +82,8 @@
                     </select>
                 </label>
                 <div class="divisor"></div>
-                <label for="e-desc"><p class="caption">Háblanos sobre el problema</p>
-                    <textarea name="e-desc" id="e-desc" placeholder="Escribe aquí..." style="resize: none;"></textarea>
+                <label for="e-desc"><p class="caption">Háblanos sobre el problema [Carácteres - Min: 100/Max: 512]</p>
+                    <textarea name="e-desc" id="e-desc" placeholder="Escribe aquí..." style="resize: none;" <?= isset($_SESSION["cache"]) ? 'autofocus="autofocus"' : '' ?>><?= isset($_SESSION["cache"]) ? $_SESSION["cache"] : '' ?><?php unset($_SESSION["cache"]) ?></textarea>
                 </label>
                 <div class="divisor"></div>
                 <label for="e-img"><p class="caption">Adjuntar una imagen del problema [Max: 2MB / Formato: .png, .jpg] <b>(Opcional)</b></p>
