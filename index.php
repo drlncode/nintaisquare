55fe1baf3c91d561f38485448f0738e45018d984
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-04-14 17:52:55 -0400

    Modoficando title.

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="NintaiSquare">
     <meta name="description" content="Te ayudamos a impulsar tu negocio de forma rápida, sencilla y gratis.">
-    <title>Bienvenido | NintaiSquare</title>
+    <title>NintaiSquare</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
     <link rel="stylesheet" href="sources/assets/styles/root.css">
     <link rel="stylesheet" href="sources/assets/styles/index-nl.css">
