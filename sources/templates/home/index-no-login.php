a8abfdb69190ab0678d96ba9ca7a20dbc0eb87b4
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-04-14 17:54:14 -0400

    Darlin: Agregados.

--- sources/templates/home/index-no-login.php
+++ sources/templates/home/index-no-login.php
@@ -2,7 +2,7 @@
     <div class="landing-content div-content-1">
         <div class="content-text">
             <div class="text-content">
-                <h1 class="main-title title-landing"><strong>NintaiSquare</strong></h1>
+                <h1 class="main-title title-landing"><strong>En NintaiSquare</strong></h1>
                 <h2 class="title-landing"><strong>Te ayudamos a impulsar tu negocio de forma rápida, sencilla y <span class="text-black">gratis</span>.</strong></h2>
                 <p class="text-landing">Crear un negocio desde cero y darle publicidad puede ser algo tedioso, por eso estamos aqui, para ayudarte a hacer crecer tu negocio de manera gratuita sin muchas complicaciones de por medio.</p>
             </div>
