1960f4eb6c5d70454b4f4b7961eeebc2b9d581b3
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-04-15 01:07:23 -0400

    Darlin: Creando apartado de soporte (19/?)

--- support/guide/index.php
+++ support/guide/index.php
@@ -29,35 +29,53 @@
         </div>
         <div class="guides-content">
             <div class="guides">
-                <div class="guide">
+                <div class="guide" id="how-to-register-store">
                     <div class="guide-header">
                         <h3 class="title"><i class="fa-solid fa-book-open-reader"></i>Cómo regístrar una tienda?</h3>
                     </div>
                     <div class="guide-content">
                         <p class="text">
-                            Para registrar una tienda en NintaiSquare es necesario que se haya regístrado, una vez hecho puede proceder con los siguientes pasos.<br><br>
+                            Para registrar una tienda en NintaiSquare es necesario que se haya regístrado, una vez hecho, puede proceder con los siguientes pasos.<br><br>
                             1. En el parte superior ubique la opción "Crear" y haga click.<br>
                             2. Siguiente, elija la opción "Regístrar tienda".<br>
-                            3. posteriormente dentro rellene los datos solicitados.<br>
+                            3. posteriormente dentro, rellene los datos solicitados.<br>
                             4. Una vez haya terminado, haga click en el botón "Registrar".<br><br>
                             <i>Nota: Tenga en cuenta que los 6 primeros campos son obligarios.</i><br><br>
                             Es recomendable que no rellene ningún campo apresuradamente y que se tome su tiempo, puesto que, los detalles no se pueden editar más adelante.
                         </p>
                     </div>
                 </div>
+                <div class="divisor"></div>
+                <div class="guide" id="how-to-register-products">
+                    <div class="guide-header">
+                        <h3 class="title"><i class="fa-solid fa-book-open-reader"></i>Cómo regístrar productos?</h3>
+                    </div>
+                    <div class="guide-content">
+                        <p class="text">
+                            Para registrar un producto para su tienda en NintaiSquare es necesario que se haya regístrado una tienda y que la misma haya sido aprobada, una vez hecho, puede proceder con los siguientes pasos.<br><br>
+                            1. En el parte superior ubique la opción "Crear" y haga click.<br>
+                            2. Siguiente, elija la opción "Regístrar producto".<br>
+                            3. posteriormente dentro, rellene los datos solicitados.<br>
+                            4. Una vez haya terminado, haga click en el botón "Registrar".<br><br>
+                            <i>Nota: Tenga en cuenta que todos los campos son obligarios.</i><br><br>
+                            Es recomendable que no rellene ningún campo apresuradamente y que se tome su tiempo, puesto que, los detalles no se pueden editar más adelante.
+                        </p>
+                    </div>
+                </div>
+                <div class="divisor"></div>
             </div>
             <div class="indice">
-                <h4 class="title-content"><i class="fa-solid fa-list-ul"></i>Índice</h4>
-                <div class="list">
-                    <ul>
-                        <li><a href="#how-to-register-store">Cómo regístrar una tienda.</a></li>
-                        <li><a href="#how-to-register-products">Cómo regístrar productos.</a></li>
-                        <li><a href="#how-see-pen-stores">Cómo ver tiendas las pendientes.</a></li>
-                        <li><a href="#how-delete-all-my-stores">Cómo eliminar todas mis tienda.</a></li>
-                        <li><a href="#how-delete-all-my-products">Cómo eliminar todas mis productos.</a></li>
-                        <li><a href="#how-change-email">Cómo cambiar mi correo.</a></li>
-                        <li><a href="#how-change-pw">Cómo cambiar mi contraseña.</a></li>
-                    </ul>
+                <div class="indice-content">
+                    <h4 class="title-content"><i class="fa-solid fa-list-ul"></i>Índice</h4>
+                    <div class="list">
+                        <a href="#how-to-register-store" class="link"><i class="fa-solid fa-circle"></i>Cómo regístrar una tienda.</a>
+                        <a href="#how-to-register-products" class="link"><i class="fa-solid fa-circle"></i>Cómo regístrar productos.</a>
+                        <a href="#how-see-pen-stores" class="link"><i class="fa-solid fa-circle"></i>Cómo ver tiendas las pendientes.</a>
+                        <a href="#how-delete-all-my-stores" class="link"><i class="fa-solid fa-circle"></i>Cómo eliminar todas mis tienda.</a>
+                        <a href="#how-delete-all-my-products" class="link"><i class="fa-solid fa-circle"></i>Cómo eliminar todas mis productos.</a>
+                        <a href="#how-change-email" class="link"><i class="fa-solid fa-circle"></i>Cómo cambiar mi correo.</a>
+                        <a href="#how-change-pw" class="link"><i class="fa-solid fa-circle"></i>Cómo cambiar mi contraseña.</a>
+                    </div>
                 </div>
             </div>
         </div>
