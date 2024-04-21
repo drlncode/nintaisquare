e1e0003a5ae3d6a9fff939781c940099ab8af3cf
Darlin Daniel Arias Méndez <darlin.informatica@gmail.com>
2024-04-21 13:06:21 -0400

    Darlin: Creando el apartado de administración (32/?)

--- sources/controller/admin/index.php
+++ sources/controller/admin/index.php
@@ -1,6 +1,7 @@
 <?php
     session_start();
     require_once("../funciones.php");
+    require_once("../pdo.php");
     noset();
     $admin = new admin_validation; //Linea 19 funciones.php
     $admin -> noadmin();
@@ -9,7 +10,87 @@
     if (isset($_GET["salir"])) {
         unset($_SESSION["USER_AUTH"]["admin_confirm"]);
         header("Location: https://nintaisquare.com/");
-        return;
+        exit;
+    }
+
+    if (isset($_GET["update"])) {
+        $all_data = [];
+
+        //*Datos de las tiendas.
+        $stores_data = [];
+
+        //Parte 1.
+        $query_stores_count = $pdo -> query("SELECT store_id FROM val_stores");
+        array_push($stores_data, ["count_total" => $query_stores_count -> rowCount()]);
+
+        //Parte 2.
+        $query_stores_accepted = $pdo -> query("SELECT id FROM history WHERE `status` = 'accepted' AND `category` = 'store'");
+        array_push($stores_data, ["stores_accepted" => $query_stores_accepted -> rowCount()]);
+
+        //Parte 3.
+        $query_stores_denied = $pdo -> query("SELECT id FROM history WHERE `status` = 'denied' AND `category` = 'store'");
+        array_push($stores_data, ["stores_denied" => $query_stores_denied -> rowCount()]);
+        
+        //Parte 4.
+        $query_stores_deleted = $pdo -> query("SELECT id FROM history WHERE `status` = 'deleted' AND `category` = 'store'");
+        $deleted_stores = $query_stores_deleted -> rowCount();
+        
+        //Parte 5.
+        $query_stores_deleted_all = $pdo -> query("SELECT id FROM history WHERE `status` = 'deleted_all' AND `category` = 'store'");
+        array_push($stores_data, ["stores_deleted" => $deleted_stores += $query_stores_deleted_all -> rowCount()]);
+        array_push($all_data, $stores_data);
+
+        //*Datos de los productos.
+        $products_data = [];
+
+        //Parte 1.
+        $query_products_count = $pdo -> query("SELECT product_id FROM val_products");
+        array_push($products_data, ["count_total" => $query_products_count -> rowCount()]);
+
+        //Parte 2.
+        $query_products_accepted = $pdo -> query("SELECT id FROM history WHERE `status` = 'accepted' AND `category` = 'product'");
+        array_push($products_data, ["products_accepted" => $query_products_accepted -> rowCount()]);
+
+        //Parte 3.
+        $query_products_denied = $pdo -> query("SELECT id FROM history WHERE `status` = 'denied' AND `category` = 'product'");
+        array_push($products_data, ["products_denied" => $query_products_denied -> rowCount()]);
+
+        //Parte 4. 
+        $query_products_deleted = $pdo -> query("SELECT id FROM history WHERE `status` = 'deleted' AND `category` = 'product'");
+        $deleted_products = $query_products_deleted -> rowCount();
+
+        //Parte 5.
+        $query_products_deleted_all = $pdo -> query("SELECT id FROM history WHERE `status` = 'deleted_all' AND `category` = 'product'");
+        array_push($products_data, ["products_deleted" => $deleted_products += $query_products_deleted_all -> rowCount()]);
+        array_push($all_data, $products_data);
+
+        //*Datos de los usuarios.
+        $users_data = [];
+
+        //Parte 1.
+        $query_users_count = $pdo -> query("SELECT user_id FROM users;");
+        array_push($users_data, ["count_total" => $query_users_count -> rowCount()]);
+
+        //Parte 2.
+        $query_admins_count = $pdo -> query("SELECT user_id FROM users WHERE admin = '1'");
+        array_push($users_data, ["count_admins_total" => $query_admins_count -> rowCount()]);
+
+        //Parte 3. 
+        $query_noadmins_count = $pdo -> query("SELECT user_id FROM users WHERE admin = '0'");
+        array_push($users_data, ["count_no_admins_total" => $query_noadmins_count -> rowCount()]);
+
+        //Parte 4.
+        $users_deleted_count = $pdo -> query("SELECT id FROM history WHERE `status` = 'deleted' AND `category` = 'user'");
+        array_push($users_data, ["count_users_deleted" => $users_deleted_count -> rowCount()]);
+        array_push($all_data, $users_data);
+
+        $json = json_encode($all_data);
+        file_put_contents("data/statistics.json", $json);
+        header("Location: index.php");
+        exit;
+    } else {
+        $statistics = json_decode("https://nintaisquare.com/sources/controller/admin/data/statistics.json", true);
+        var_dump($statistics);
     }
 ?>
 <!DOCTYPE html>
